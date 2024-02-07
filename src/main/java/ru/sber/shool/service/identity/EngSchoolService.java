2ac761887b496b8ec9970b24f94f256fd0b63bf3
Ivan <ivan.nikushin.99@mail.ru>
2024-02-08 00:07:38 +0300

    Классы с аннотацией ServiceSchool аннотированы @Component, чтобы попали в контекст приложения

--- src/main/java/ru/sber/shool/service/identity/EngSchoolService.java
+++ src/main/java/ru/sber/shool/service/identity/EngSchoolService.java
@@ -1,8 +1,10 @@
 package ru.sber.shool.service.identity;
 
 
+import org.springframework.stereotype.Component;
 import ru.sber.shool.service.ServiceSchool;
 
+@Component
 @ServiceSchool(serviceName = "School1", host = "google.com")
 public class EngSchoolService {
 }
--- src/main/java/ru/sber/shool/service/identity/RuSchoolService.java
+++ src/main/java/ru/sber/shool/service/identity/RuSchoolService.java
@@ -1,8 +1,10 @@
 package ru.sber.shool.service.identity;
 
 
+import org.springframework.stereotype.Component;
 import ru.sber.shool.service.ServiceSchool;
 
+@Component
 @ServiceSchool(serviceName = "School2", host = "yandex.ru")
 public class RuSchoolService {
 }
