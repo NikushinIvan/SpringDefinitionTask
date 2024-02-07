c1a8b876bdcdaaa41174c4e4c2c339a9620da2c9
Ivan <ivan.nikushin.99@mail.ru>
2024-02-07 23:56:00 +0300

    Добавлена начальная конфигурация приложения

--- src/main/java/ru/sber/InheritanceDemo.java
+++ src/main/java/ru/sber/InheritanceDemo.java
@@ -2,10 +2,14 @@ package ru.sber;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan("ru.sber.shool")
 public class InheritanceDemo {
 
 	public static void main(String... args) {
-		ApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(InheritanceDemo.class);
 	}
 }
