0d06a52ceaefb1f4e976f1719a1697dced73b605
Nikushin I.M <ivan.nikushin.99@mail.ru>
2024-02-08 10:20:55 +0300

    Реализация фабрики ServiceCompositeBeanDefinition

--- src/main/java/ru/sber/shool/factory/ServiceCompositeBeanDefinition.java
+++ src/main/java/ru/sber/shool/factory/ServiceCompositeBeanDefinition.java
@@ -1,13 +1,15 @@
 package ru.sber.shool.factory;
 
 import org.springframework.stereotype.Component;
+import ru.sber.shool.service.Logger;
+import ru.sber.shool.service.Monitoring;
 import ru.sber.shool.service.Service;
 
 @Component
 public class ServiceCompositeBeanDefinition {
 
 
-    public Service create() {
-        return null;
+    public Service create(Logger logger, Monitoring monitoring) {
+        return new Service(logger, monitoring);
     }
 }
