3cbc5aba5259d867f1616519b3806e0bcd031168
Nikushin I.M <ivan.nikushin.99@mail.ru>
2024-02-08 10:19:43 +0300

    Определение класса Service

--- src/main/java/ru/sber/shool/service/Service.java
+++ src/main/java/ru/sber/shool/service/Service.java
@@ -2,4 +2,19 @@ package ru.sber.shool.service;
 
 public class Service {
 
+    private Logger logger;
+    private Monitoring monitoring;
+
+    public Service(Logger logger, Monitoring monitoring) {
+        this.logger = logger;
+        this.monitoring = monitoring;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    public Monitoring getMonitoring() {
+        return monitoring;
+    }
 }
